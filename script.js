b3f128d29254fd1a72895772b46ad96b65f4f016
Festusx <uzorchimedu@gmail.com>
2023-09-10 21:51:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+const getCurrentTimeDate = () => {
+    let currentTimeDate = new Date();
+
+    var weekday = new Array(7);
+    weekday[0] = "SUNDAY";
+    weekday[1] = "MONDAY";
+    weekday[2] = "TUESDAY";
+    weekday[3] = "WEDNESDAY";
+    weekday[4] = "THURSDAY";
+    weekday[5] = "FRIDAY";
+    weekday[6] = "SATURDAY";
+
+    var month = new Array();
+    month[0] = "JANUARY";
+    month[1] = "FEBRUARY";
+    month[2] = "MARCH";
+    month[3] = "APRIL";
+    month[4] = "May";
+    month[5] = "JUNE";
+    month[6] = "JULY";
+    month[7] = "AUGUST";
+    month[8] = "SEPTEMBER";
+    month[9] = "OCTOBER";
+    month[10] = "NOVEMBER";
+    month[11] = "DECEMBER";
+    var hours   =  currentTimeDate.getHours();
+
+        var minutes =  currentTimeDate.getMinutes();
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+
+         var AMPM = hours >= 12 ? 'PM' : 'AM';
+
+        if(hours === 12){
+            hours=12;
+
+        }else{
+
+            hours = hours%12;
+
+        }
+
+        var currentTime = `${hours}:${minutes}${AMPM}`;
+        var currentDay = weekday[currentTimeDate.getDay()];
+
+        var currentMonth = month[currentTimeDate.getMonth()];
+        var CurrentYear = currentTimeDate.getFullYear();
+
+        var fullDate = `${currentMonth} ${CurrentYear}`;
+
+
+        document.getElementById("currentUTCTime").innerHTML = currentTime;
+        document.getElementById("currentDayOfTheWeek").innerHTML = currentDay;
+        setTimeout(getCurrentTimeDate, 500);
+
+    }
+    getCurrentTimeDate();
