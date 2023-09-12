fe03699ac6ea9b7a465217c6e0df74d85f4474e6
Festusx <uzorchimedu@gmail.com>
2023-09-12 11:28:12 +0100

    v

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 const c = new Date();
@@ -7,3 +8,15 @@ document.getElementById("currentDayOfTheWeek").innerHTML = day;
 const d = new Date();
 let ms = d.getUTCMilliseconds();
 document.getElementById("currentUTCTime").innerHTML = ms;
+
+const f = new Date();
+let hrs = d.getHours();
+document.getElementById("Hours").innerHTML = hrs;
+
+const g = new Date();
+let min = d.getMinutes();
+document.getElementById("Minutes").innerHTML = min;
+
+const h = new Date();
+let sec = d.getSeconds();
+document.getElementById("Seconds").innerHTML = sec;
